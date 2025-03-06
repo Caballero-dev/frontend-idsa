4585c8d955561f4d4e6d6c4566ef18ff2a292152
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-03-06 09:48:28 -0600

    Enhance password update functionality with error handling and success messages in profile details component

--- src/app/feactures/profile/components/profile-details/profile-details.component.ts
+++ src/app/feactures/profile/components/profile-details/profile-details.component.ts
@@ -4,20 +4,26 @@ import { ButtonModule } from 'primeng/button';
 import { InputTextComponent } from '../../../../shared/components/input-text/input-text.component';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '../../../../utils/form.utils';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-profile-details',
   standalone: true,
-  imports: [CommonModule, ButtonModule, InputTextComponent, ReactiveFormsModule],
+  imports: [CommonModule, ButtonModule, InputTextComponent, ReactiveFormsModule, MessageModule, ToastModule],
   templateUrl: './profile-details.component.html',
   styleUrl: './profile-details.component.scss',
+  providers: [MessageService],
 })
 export class ProfileDetailsComponent implements OnInit {
   viewMyProfile: boolean = true;
   viewUpdatePassword: boolean = false;
+  errorUpdatePassword: boolean = false;
   formUtils = FormUtils;
 
   fb: FormBuilder = inject(FormBuilder);
+  messageService: MessageService = inject(MessageService);
 
   updatePasswordForm = this.fb.group(
     {
@@ -58,10 +64,43 @@ export class ProfileDetailsComponent implements OnInit {
 
   updatePassword(): void {
     if (this.updatePasswordForm.valid) {
-      console.log('Contraseña actualizada', this.updatePasswordForm.value);
+      this.messageToast(
+        'success',
+        'pi pi-check',
+        true,
+        'pi pi-times',
+        false,
+        'Contraseña actualizada',
+        'La contraseña ha sido actualizada correctamente',
+        3000
+      );
+      this.errorUpdatePassword = false;
+      this.updatePasswordForm.reset();
     } else {
-      console.log('No se puede actualizar la contraseña', this.updatePasswordForm.errors);
+      this.errorUpdatePassword = true;
       this.updatePasswordForm.markAllAsTouched();
     }
+  }
+
+  messageToast(
+    severity?: 'success' | 'info' | 'warn' | 'error' | 'secondary' | 'contrast',
+    icon?: string,
+    closable?: boolean,
+    closeIcon?: string,
+    sticky?: boolean,
+    summary?: string,
+    detail?: string,
+    life?: number
+  ): void {
+    this.messageService.add({
+      severity: severity,
+      icon: icon,
+      closable: closable,
+      closeIcon: closeIcon,
+      sticky: sticky,
+      summary: summary,
+      detail: detail,
+      life: life,
+    });
   }
 }
