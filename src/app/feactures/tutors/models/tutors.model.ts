bd7eae149a3e5704c75efc65f77b1aad7c8cb24b
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-03-10 23:35:56 -0600

    Refactor tutor form and list components for improved functionality and user experience

---
+++ src/app/feactures/tutors/models/tutors.model.ts
@@ -0,0 +1,15 @@
+export interface Tutor {
+  tutorId: number;
+  name: string;
+  firstSurname: string;
+  secondSurname: string;
+  employeeCode: string;
+  phoneNumber: string;
+  email: string;
+}
+
+export interface EmitterDialogTutor {
+  isOpen: boolean;
+  message: 'save' | 'edit' | 'close';
+  tutor: Tutor | null;
+}
