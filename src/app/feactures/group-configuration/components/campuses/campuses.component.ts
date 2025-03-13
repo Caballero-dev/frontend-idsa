74b4a723d9a67f589f400a5cf619425618258ce1
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-03-12 22:08:50 -0600

    Implement campus management functionality with create, edit, and delete capabilities

--- src/app/feactures/group-configuration/components/campuses/campuses.component.ts
+++ src/app/feactures/group-configuration/components/campuses/campuses.component.ts
@@ -1,10 +1,173 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Column, TableUtils } from '../../../utils/table.utils';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Campus } from '../../models/campuses.model';
+import { CampusesTestService } from '../../tests/campuses-test.service';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { FormUtils } from '../../../../utils/form.utils';
+import { InputTextComponent } from '../../../../shared/components/input-text/input-text.component';
+import { DialogModule } from 'primeng/dialog';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+
 @Component({
   selector: 'app-campuses',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    ToolbarModule,
+    InputGroupModule,
+    InputTextModule,
+    TableModule,
+    DialogModule,
+    ToastModule,
+    ConfirmDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextComponent,
+  ],
   templateUrl: './campuses.component.html',
   styleUrl: './campuses.component.scss',
-  providers: [],
+  providers: [ConfirmationService, MessageService],
 })
-export class CampusesComponent {}
+export class CampusesComponent implements OnInit {
+  searchCampusValue: string = '';
+  isLoading: boolean = true;
+  campusDialogVisible: boolean = false;
+  isCreateCampus: boolean = true;
+
+  cols: Column[] = [{ field: 'name', header: 'Nombre', sortable: true }];
+  campuses!: Campus[];
+  selectedCampus: Campus | null = null;
+
+  tableUtils = TableUtils;
+  formUtils = FormUtils;
+
+  private confirmationService: ConfirmationService = inject(ConfirmationService);
+  private messageService: MessageService = inject(MessageService);
+  private campusTestService = inject(CampusesTestService);
+  private fb: FormBuilder = inject(FormBuilder);
+
+  campusForm = this.fb.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(100),
+        Validators.pattern(this.formUtils.onlyLettersPattern),
+      ],
+    ],
+  });
+
+  ngOnInit(): void {
+    this.loadCampuses();
+  }
+
+  loadCampuses(): void {
+    this.campuses = this.campusTestService.getData();
+    this.isLoading = false;
+  }
+
+  saveOrUpdateCampus(): void {
+    if (this.isCreateCampus) {
+      this.saveCampus();
+    } else {
+      this.updateCampus();
+    }
+  }
+
+  saveCampus(): void {
+    if (this.campusForm.valid) {
+      let campus: Campus = {
+        campusId: Math.random(),
+        name: this.campusForm.value.name as string,
+      };
+
+      this.campuses = [...this.campuses, campus];
+      this.campusDialogVisible = false;
+      this.clearCampusForm();
+      this.showToast('success', 'Campus creado', 'El campus ha sido creado correctamente');
+    } else {
+      this.campusForm.markAllAsTouched();
+    }
+  }
+
+  updateCampus(): void {
+    if (this.campusForm.valid && this.selectedCampus) {
+      let campus: Campus = {
+        campusId: this.selectedCampus.campusId,
+        name: this.campusForm.value.name as string,
+      };
+
+      this.campuses = this.campuses.map((c: Campus) => (c.campusId === campus.campusId ? campus : c));
+      this.campusDialogVisible = false;
+      this.clearCampusForm();
+      this.showToast('success', 'Campus actualizado', 'El campus ha sido actualizado correctamente');
+    } else {
+      this.campusForm.markAllAsTouched();
+    }
+  }
+
+  editCampus(campus: Campus): void {
+    this.selectedCampus = campus;
+    this.isCreateCampus = false;
+    this.campusForm.patchValue({
+      name: campus.name,
+    });
+    this.campusDialogVisible = true;
+  }
+
+  deleteCampus(campus: Campus): void {
+    this.confirmationService.confirm({
+      message: `¿Estás seguro que deseas eliminar el campus seleccionado?<br><br>Nombre: ${campus.name}`,
+      header: 'Confirmar',
+      closable: false,
+      closeOnEscape: false,
+      icon: 'pi pi-exclamation-triangle',
+      rejectButtonProps: {
+        label: 'Cancelar',
+        severity: 'secondary',
+        outlined: true,
+      },
+      acceptButtonProps: {
+        label: 'Aceptar',
+      },
+      accept: () => {
+        this.campuses = this.campuses.filter((c: Campus) => c.campusId !== campus.campusId);
+        this.showToast('success', 'Campus eliminado', 'El campus ha sido eliminado correctamente');
+      },
+      reject: () => {
+        this.showToast('error', 'Eliminación cancelada', 'Has cancelado la eliminación del campus');
+      },
+    });
+  }
+
+  closeDialog(): void {
+    this.campusDialogVisible = false;
+    this.clearCampusForm();
+    this.showToast('error', 'Operación cancelada', 'Has cancelado la operación');
+  }
+
+  clearCampusForm(): void {
+    this.campusForm.reset();
+    this.selectedCampus = null;
+    this.isCreateCampus = true;
+  }
+
+  showToast(severity: 'success' | 'error' | 'info', summary: string, detail: string): void {
+    this.messageService.add({
+      severity,
+      icon: severity === 'success' ? 'pi pi-check-circle' :
+        severity === 'error' ? 'pi pi-times-circle' : 'pi pi-info-circle',
+      summary,
+      detail,
+      life: 3000,
+    });
+  }
+}
