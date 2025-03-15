b7b6789c0dea63af19a938a855ab24950f1bffa2
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-03-14 19:56:54 -0600

    Refactor PrimeNG configuration to use centralized setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,44 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import { BlueAuraPreset } from '../assets/themes/blue-aura-theme';
+import { providePrimeNGConfig } from './core/config/primeng.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: BlueAuraPreset,
-        options: {
-          darkModeSelector: false,
-        },
-      },
-      ripple: true,
-      translation: {
-        firstDayOfWeek: 1,
-        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-        dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
-        dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
-        monthNames: [
-          'Enero',
-          'Febrero',
-          'Marzo',
-          'Abril',
-          'Mayo',
-          'Junio',
-          'Julio',
-          'Agosto',
-          'Septiembre',
-          'Octubre',
-          'Noviembre',
-          'Diciembre',
-        ],
-        monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
-        today: 'Hoy',
-        clear: 'Limpiar',
-      },
-    }),
+    providePrimeNGConfig(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
   ],
