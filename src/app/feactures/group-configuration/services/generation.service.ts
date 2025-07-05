0492cbc9d555492c4c01fa00b99fbf573366b80f
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-07-05 16:31:32 -0600

    Refactor: implement search functionality in generations list and enhance data handling

--- src/app/feactures/group-configuration/services/generation.service.ts
+++ src/app/feactures/group-configuration/services/generation.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { ApiResponse } from '../../../core/models/ApiResponse.model';
 import { Observable } from 'rxjs';
 import { GenerationRequest, GenerationResponse } from '../models/generation.model';
+import { hasText } from '../../../utils/string.utils';
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +13,17 @@ export class GenerationService {
   private readonly API_URL = `${environment.URL_API}/admin/generations`;
   private http: HttpClient = inject(HttpClient);
 
-  getAllGenerations(page: number = 0, size: number = 20): Observable<ApiResponse<GenerationResponse[]>> {
-    return this.http.get<ApiResponse<GenerationResponse[]>>(this.API_URL, {
-      params: {
-        page,
-        size,
-      },
-    });
+  getAllGenerations(
+    page: number = 0,
+    size: number = 20,
+    search?: string | null
+  ): Observable<ApiResponse<GenerationResponse[]>> {
+    const params: any = {
+      page,
+      size,
+      ...(hasText(search) && { search }),
+    };
+    return this.http.get<ApiResponse<GenerationResponse[]>>(this.API_URL, { params });
   }
 
   createGeneration(generation: GenerationRequest): Observable<ApiResponse<GenerationResponse>> {
